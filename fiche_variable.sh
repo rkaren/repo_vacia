cc761a0afac100c0fd414b3713b391eea3f2972e
rkaren <karen20mm@gmail.com>
2019-11-18 14:19:10 +0100

    he creado 2 script

---
+++ fiche_variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+myvar="hola"
+echo $myvar#accedo al comntenido
+echo "$myvar"#accedo al comntenido
+echo '$myvar'#accedo al comntenido
+echo \$myvar#accedo al comntenido
---
+++ ficherito.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#description
+#author
+echo "hola mundo"
+exit 0#yo osy un comentario al final de linea
+#exit 0 indica que todo termino bien. esta linea no se ejecutara
